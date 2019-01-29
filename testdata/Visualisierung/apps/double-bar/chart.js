697546febf5069f1c38e100cd22a0b5b27ff15a0
kummerloewej <kummerloewej@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-01-29 13:19:16 +0000

    add another example: double bar diagram with additional bullets to indicate overall saldo
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@37 366f23da-3ab8-43e7-bb03-06f30a681aac

--- testdata/Visualisierung/apps/double-bar/chart.js
+++ testdata/Visualisierung/apps/double-bar/chart.js
@@ -30,17 +30,17 @@ require(["vendor/amcharts4/core", "vendor/amcharts4/charts", 'vendor/amcharts4/t
 
 		// Create series
 		var maleSeries = maleChart.series.push(new am4charts.ColumnSeries());
-		maleSeries.dataFields.valueX = "hin";
-		maleSeries.fill = am4core.color("#119C11");
+		maleSeries.dataFields.valueX = "weg";
+		maleSeries.fill = am4core.color("#ff0000");
 		maleSeries.dataFields.categoryY = "id";
-		maleSeries.columns.template.tooltipText = "Zuzug OT {categoryY}: {valueX}";
+		maleSeries.columns.template.tooltipText = "Wegzug OT {categoryY}: {valueX}";
 
 
 		var femaleChart = mainContainer.createChild(am4charts.XYChart);
 		femaleChart.paddingLeft = 0;
 		femaleChart.data = data;
 		// Use only absolute numbers
-		maleChart.numberFormatter.numberFormat = "#.#s";
+		femaleChart.numberFormatter.numberFormat = "#.#s";
 
 		// Create axes
 		var femaleCategoryAxis = femaleChart.yAxes.push(new am4charts.CategoryAxis());
@@ -55,12 +55,37 @@ require(["vendor/amcharts4/core", "vendor/amcharts4/charts", 'vendor/amcharts4/t
 
 		// Create series
 		var femaleSeries = femaleChart.series.push(new am4charts.ColumnSeries());
-		femaleSeries.dataFields.valueX = "weg";
-		femaleSeries.fill = am4core.color("#ff0000");
+		femaleSeries.dataFields.valueX = "hin";
+		femaleSeries.fill = am4core.color("#119C11");
 		femaleSeries.stroke = femaleSeries.fill;
-		femaleSeries.columns.template.tooltipText = "Wegzug OT {categoryY}: {valueX}";
+		femaleSeries.columns.template.tooltipText = "Zuzug OT {categoryY}: {valueX}";
 		femaleSeries.dataFields.categoryY = "id";
 
+		// Create series
+		var saldoSeries = femaleChart.series.push(new am4charts.LineSeries());
+		saldoSeries.dataFields.valueX = "saldo";
+		saldoSeries.dataFields.categoryY = "id";
+		saldoSeries.stroke = am4core.color("#0000ff");
+		saldoSeries.strokeOpacity = 0;
+		saldoSeries.xAxis = femaleValueAxis;
+		saldoSeries.yAxis = femaleCategoryAxis;
+		saldoSeries.data = data;
+
+		var bullet1 = saldoSeries.bullets.push(new am4charts.CircleBullet());
+		bullet1.tooltipText = "Saldo OT {categoryY}: {valueX}";
+
+		var saldoSeries2 = maleChart.series.push(new am4charts.LineSeries());
+		saldoSeries2.dataFields.valueX = "saldo2";
+		saldoSeries2.dataFields.categoryY = "id";
+		//saldoSeries2.columns.template.tooltipText = "Saldo OT {categoryY}: {valueX}";
+		saldoSeries2.stroke = am4core.color("#0000ff");
+		saldoSeries2.strokeOpacity = 0;
+		saldoSeries2.xAxis = maleValueAxis;
+		saldoSeries2.yAxis = maleCategoryAxis;
+		saldoSeries2.data = data;
+
+		var bullet2 = saldoSeries2.bullets.push(new am4charts.CircleBullet());
+		bullet2.tooltipText = "Saldo OT {categoryY}: -{valueX}";
 
 	}
 );
