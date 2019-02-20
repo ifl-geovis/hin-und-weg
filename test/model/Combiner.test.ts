115fbba8bd3079d71dc51bdc056872d393904e5d
rpu <rpu@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-02-20 10:43:54 +0000

    Linted Combiner tests
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@70 366f23da-3ab8-43e7-bb03-06f30a681aac

--- test/model/Combiner.test.ts
+++ test/model/Combiner.test.ts
@@ -1,59 +1,61 @@
-import { expect } from "chai"
-import * as Debug from '../../src/debug'
-import Combiner from '../../src/model/Combiner';
-import Geodata from '../../src/model/Geodata'
-import Tabledata from '../../src/model/Tabledata'
-import { callbackify } from "util";
+import { expect } from "chai";
+import * as Debug from "../../src/debug";
+import Combiner from "../../src/model/Combiner";
+import Geodata from "../../src/model/Geodata";
+import Tabledata from "../../src/model/Tabledata";
 
-Debug.on()
+Debug.on();
 
-describe('Combiner takes Geodata and Tabledata and combines Geodata fields and Matrix cells',() => {
+describe("Combiner takes Geodata and Tabledata and combines Geodata fields and Matrix cells", () => {
 
-    let combiner: Combiner     
+    let combiner: Combiner;
 
-    beforeEach((done)=>{
-        Geodata.read('./testdata/ot.shp', (geodata: Geodata) => {
-            combiner = new Combiner(geodata,['Jahr','Von','Nach'])
-            combiner.setGeodataId('OT').setGeodataSelector('Name')
-            done()
-        })
-    })
+    beforeEach((done) => {
+        Geodata.read("./testdata/ot.shp", (geodata: Geodata) => {
+            combiner = new Combiner(geodata,["Jahr","Von","Nach"]);
+            combiner.setGeodataId("OT").setGeodataSelector("Name");
+            done();
+        });
+    });
 
-    it('Is created with Geodata and Table dimensions',() => {        
-        expect(combiner).not.null    
-        expect(combiner.getTableCount()).is.equal(0)        
-    })
+    it("Is created with Geodata and Table dimensions", () => {
+        expect(combiner).not.equals(null);
+        expect(combiner.getTableCount()).is.equal(0);
+    });
 
-    it('Can add Tabledata and provides Table names, rowNames, columnNames',(done) => {
-        let name = './testdata/Leipzig-OT Umzüge_2002.csv'
+    it("Can add Tabledata and provides Table names, rowNames, columnNames",(done) => {
+        const name = "./testdata/Leipzig-OT Umzüge_2002.csv";
+        Tabledata.read(name, (tabledata) => {
+            combiner.addTable("Jahr", "2002", tabledata.getTabledataBy(
+                    [ 3, tabledata.getRowCount() - 2], [1, tabledata.getColumnCount() - 1],
+            ));
+            expect(combiner.getTableCount()).is.equal(1);
+            expect(combiner.getTableNames()).is.deep.equal(["2002"]);
+            expect(combiner.getRowNamesFor("2002")).length(63,"Row names length should be 63");
+            expect(combiner.getRowNamesFor("2002")).contains("Zentrum-Nordwest").and.contains("Zentrum-Ost");
+            expect(combiner.getColumnNamesFor("2002")).length(63,"Column names length should be 63");
+            expect(combiner.getColumnNamesFor("2002")).contains("Zentrum-Südost").and.contains("Zentrum-Süd");
+            done();
+        });
+    });
+
+    it("Can query by all dimensions",(done) => {
+        const name = "./testdata/Leipzig-OT Umzüge_2002.csv";
         Tabledata.read(name,(tabledata) => {
-            combiner.addTable('Jahr','2002',tabledata.getTabledataBy([3,tabledata.getRowCount()-2],[1,tabledata.getColumnCount()-1]))
-            expect(combiner.getTableCount()).is.equal(1)
-            expect(combiner.getTableNames()).is.deep.equal(['2002'])
-            expect(combiner.getRowNamesFor('2002')).length(63,'Row names length should be 63')
-            expect(combiner.getRowNamesFor('2002')).contains('Zentrum-Nordwest').and.contains('Zentrum-Ost')
-            expect(combiner.getColumnNamesFor('2002')).length(63,'Column names length should be 63')
-            expect(combiner.getColumnNamesFor('2002')).contains('Zentrum-Südost').and.contains('Zentrum-Süd')
-            done()
-        })
-    })
-
-    it('Can query by all dimensions',(done) => {
-        let name = './testdata/Leipzig-OT Umzüge_2002.csv'
-        Tabledata.read(name,(tabledata) => {
-            combiner.addTable('Jahr','2002',tabledata.getTabledataBy([3,tabledata.getRowCount()-2],[1,tabledata.getColumnCount()-1]))            
-            let year2002 = combiner.query({'Jahr':['2002']})
-            expect(year2002).length(63*63)
-            expect(year2002[0].value).equal(32)
-            expect(year2002[0].property[0].name).equal('Jahr')
-            expect(year2002[0].property[0].value).equal('2002')
-            expect(year2002[0].property[1].name).equal('Von')
-            expect(year2002[0].property[1].value).equal('Zentrum')
-            expect(year2002[0].property[2].name).equal('Nach')
-            expect(year2002[0].property[2].value).equal('Zentrum')
-            expect('Not implemented',"Convert Result to Tabledata ?").equal("Is implemented")
-            done()
-        })
-    })
-    
+            combiner.addTable("Jahr", "2002", tabledata.getTabledataBy(
+                [3, tabledata.getRowCount() - 2],[1, tabledata.getColumnCount() - 1]
+            ));
+            const year2002 = combiner.query({Jahr: ["2002"]});
+            expect(year2002).length(63 * 63);
+            expect(year2002[0].value).equal(32);
+            expect(year2002[0].property[0].name).equal("Jahr");
+            expect(year2002[0].property[0].value).equal("2002");
+            expect(year2002[0].property[1].name).equal("Von");
+            expect(year2002[0].property[1].value).equal("Zentrum");
+            expect(year2002[0].property[2].name).equal("Nach");
+            expect(year2002[0].property[2].value).equal("Zentrum");
+            expect("Not implemented","Convert Result to Tabledata ?").equal("Is implemented");
+            done();
+        });
+    });
 })
