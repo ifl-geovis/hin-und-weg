df69d9c255dd574ea63642632d1d09cd4f2d8bd5
rpu <rpu@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-03-08 09:17:57 +0000

    Linted
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@92 366f23da-3ab8-43e7-bb03-06f30a681aac

--- test/sandbox.test.ts
+++ test/sandbox.test.ts
@@ -1,8 +1,8 @@
-import { expect } from "chai"
+import { expect } from "chai";
 
-describe('Common tests and evaluations', ()=> {
+describe("Common tests and evaluations", () => {
     it("Simple Calculations",() => {
-        expect(0.3*366).equal(109.8)
-        expect(33/8).equal(4.125)
-    })
-})
+        expect(0.3 * 366).equal(109.8);
+        expect(33 / 8).equal(4.125);
+    });
+});
