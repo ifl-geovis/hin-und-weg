c9db6a46f139d73af6b36d0cf9614908c52cfa24
rpu <rpu@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-03-08 09:31:59 +0000

    Refactored Aggregator
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@93 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/model/Aggregator.ts
+++ src/model/Aggregator.ts
@@ -4,35 +4,35 @@ import * as Debug from "../debug";
 
 Debug.on();
 
-// TODO: Refactor values hharmonization
 // TODO: More than 2 Dimensions (in this case years)
 export default class Aggregator {
 
     private operation: string;
     private defaultValue = R.curry((defaultNum: number, num: number) => isNaN(num) ? defaultNum : num);
     private rejectNaNs  = R.filter((result: Result<number>) => !isNaN(result.value));
+    private toValues = R.map((result: Result<number>) => result.value);
 
     constructor(operation: string) {
         this.operation = operation;
     }
 
     public aggregate(data: Array<Result<number>>): number {
-       if (this.operation === "Summe" ){
+       if (this.operation === "Summe" ) {
            return this.sum(data);
        }
-       if (this.operation === "Anzahl" ){
+       if (this.operation === "Anzahl" ) {
          return data.length;
        }
-       if (this.operation === "Mittelwert" ){
+       if (this.operation === "Mittelwert" ) {
           return this.mean(data);
        }
-       if (this.operation === "Medianwert" ){
+       if (this.operation === "Medianwert" ) {
         return this.median(data);
        }
-       if (this.operation === "Minimum" ){
+       if (this.operation === "Minimum" ) {
         return this.min(data);
        }
-       if (this.operation === "Maximum" ){
+       if (this.operation === "Maximum" ) {
         return this.max(data);
        }
        return data.length;
@@ -44,22 +44,20 @@ export default class Aggregator {
     }
 
     private mean(data: Array<Result<number>>) {
-        const values = R.map((result) => result.value, R.filter((result) => !isNaN(result.value), data));
-        return R.mean(values);
+        return R.mean(this.toValues(this.rejectNaNs(data)));
     }
 
     private median(data: Array<Result<number>>) {
-        const values = R.map((result) => result.value, R.filter((result) => !isNaN(result.value), data));
-        return R.median(values);
+        return R.median(this.toValues(this.rejectNaNs(data)));
     }
 
     private min(data: Array<Result<number>>) {
-        const values = R.map((result) => result.value, R.filter((result) => !isNaN(result.value), data));
+        const values = this.toValues(this.rejectNaNs(data));
         return R.reduce(R.min, Number.MAX_VALUE, values);
     }
 
     private max(data: Array<Result<number>>) {
-        const values = R.map((result) => result.value, R.filter((result) => !isNaN(result.value), data));
+        const values = this.toValues(this.rejectNaNs(data));
         return R.reduce(R.max, Number.MIN_VALUE, values);
     }
 }
