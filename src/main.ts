4a7d3259cd62b52ef1974c0f939bf86d232268b6
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-04-15 09:45:10 +0000

    reformatting, reenable dev-tools, quit on mac
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@111 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,34 @@ import * as Debugger from "./debug";
 
 let mainWindow: Electron.BrowserWindow;
 
-function createWindow() {
+function createWindow()
+{
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    height: 960,
-    width: 1400,
-    // tslint:disable-next-line: object-literal-sort-keys
-    title: "hin&weg " + process.env.npm_package_version,
-  });
+	// Create the browser window.
+	mainWindow = new BrowserWindow(
+		{
+			height: 960,
+			width: 1400,
+			// tslint:disable-next-line: object-literal-sort-keys
+			title: "hin&weg " + process.env.npm_package_version,
+		});
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+	// and load the index.html of the app.
+	mainWindow.loadFile(path.join(__dirname, "../index.html"));
 
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools({mode: "undocked"});
+	// set empty menu bar (deactivates menu)
+	mainWindow.setMenu(null);
 
-  // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-  });
+	// Open the DevTools.
+	mainWindow.webContents.openDevTools({mode: "undocked"});
+
+	// Emitted when the window is closed.
+	mainWindow.on("closed", () =>
+		{
+			// Dereference the window object, usually you would store windows
+			// in an array if your app supports multi windows, this is the time
+			// when you should delete the corresponding element.
+		});
 }
 
 // This method will be called when Electron has finished
@@ -34,21 +40,24 @@ function createWindow() {
 app.on("ready", createWindow)
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+app.on("window-all-closed", () =>
+	{
+		// On OS X it is common for applications and their menu bar
+		// to stay active until the user quits explicitly with Cmd + Q
+		//if (process.platform !== "darwin") {
+		app.quit();
+		//}
+	});
 
-app.on("activate", () => {
-  // On OS X it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+app.on("activate", () =>
+	{
+		// On OS X it"s common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (mainWindow === null)
+		{
+			createWindow();
+		}
+	});
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
