712e4ee4f7dabf12dd7d252bddbb90b9738ac0f4
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-05-13 13:36:47 +0000

    flip colors for negative/positive values
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@160 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/geo/MapView.tsx
+++ src/view/geo/MapView.tsx
@@ -24,11 +24,11 @@ export default class MapView extends React.Component<IMapViewProps, IMapViewStat
 {
 
 	// Taken from http://colorbrewer2.org/
-	// http://colorbrewer2.org/?type=sequential&scheme=PuBu&n=9
-	protected colors = ["#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"];
-	protected neutral_color = '#ffffff';
 	// http://colorbrewer2.org/?type=sequential&scheme=YlOrRd&n=9
-	protected negative_colors = ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'];
+	protected colors = ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'];
+	// http://colorbrewer2.org/?type=sequential&scheme=PuBu&n=9
+	protected negative_colors = ["#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"];
+	protected neutral_color = '#ffffff';
 
 	constructor(props: IMapViewProps)
 	{
