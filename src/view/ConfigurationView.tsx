1b8338533b66e6425d0d0f16f152abba53155ac4
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-06-24 13:46:07 +0000

    cleanup
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@167 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/ConfigurationView.tsx
+++ src/view/ConfigurationView.tsx
@@ -9,128 +9,137 @@ import FileInput from "./input/FileInput";
 import Geodata from "../model/Geodata";
 import Tabledata from "../model/Tabledata";
 
-export interface IConfigurationProps {
-    db: alaSQLSpace.AlaSQL;
-    geodata: Geodata | null;
-    setGeodata: (geodata: Geodata) => void;
-    addYear: (year: string) => void;
+export interface IConfigurationProps
+{
+	db: alaSQLSpace.AlaSQL;
+	geodata: Geodata | null;
+	setGeodata: (geodata: Geodata) => void;
+	addYear: (year: string) => void;
 }
 
-interface IConfigurationState {
-    geoId: string | null;
-    geoName: string | null;
-    tablefiles: string[];
-    geodatafile: string;
+interface IConfigurationState
+{
+	geoId: string | null;
+	geoName: string | null;
+	tablefiles: string[];
+	geodatafile: string;
 }
 
-export default class App extends React.Component<IConfigurationProps, IConfigurationState> {
+export default class App extends React.Component<IConfigurationProps, IConfigurationState>
+{
 
-    constructor(props: IConfigurationProps) {
-        super(props);
-        this.onSelectGeodataFile = this.onSelectGeodataFile.bind(this);
-        this.onSelectTabledataFiles = this.onSelectTabledataFiles.bind(this);
-        this.state = {
-            geoId: "OT",
-            geoName: "Name",
-            geodatafile: "",
-            tablefiles: [],
-        };
-    }
+	constructor(props: IConfigurationProps)
+	{
+		super(props);
+		this.onSelectGeodataFile = this.onSelectGeodataFile.bind(this);
+		this.onSelectTabledataFiles = this.onSelectTabledataFiles.bind(this);
+		this.state =
+		{
+			geoId: "OT",
+			geoName: "Name",
+			geodatafile: "",
+			tablefiles: [],
+		};
+	}
 
-    public render(): JSX.Element {
-        let geoFieldOptions;
-        if (this.props.geodata) {
-            geoFieldOptions = R.map((field) =>  {
-                    return { label: field, value: field };
-            } , this.props.geodata.fields());
-        }
-        let geodatafile = <div className="p-col-12"/>;
-        if (this.state.geodatafile ) {
-            geodatafile = <div className="p-col-8">{this.state.geodatafile}</div>;
-        }
-        const tablesfiles = R.map((tablefile) => {
-            return (<div key={tablefile} className="p-col-12">{tablefile}</div>);
-        }, this.state.tablefiles);
-        return (
-            <div>
-                <Panel header="1. Geodaten">
-                    <div className="p-grid p-justify-around">
-                     <div className="p-col-12">
-                       <FileInput label="Shape Datei auswählen..." filesSelected={this.onSelectGeodataFile} disabled={false}/>
-                     </div>
-                    {geodatafile}
-                    <Dropdown className="p-col-6" key="geoId"
-                              value={this.state.geoId} options={geoFieldOptions} disabled={this.props.geodata == null}
-                              placeholder="ID Spalte auswählen"
-                              onChange={(e) => {this.setState({geoId: e.value}); } }/>
-                    <Dropdown className="p-col-6" key="geoName"
-                              value={this.state.geoName} options={geoFieldOptions} disabled={this.props.geodata == null}
-                              placeholder="Namenspalte auswählen"
-                              onChange={(e) => {this.setState({geoName: e.value}); } }/>
-                    </div>
-                </Panel>
-                <Panel header="2. Tabellendaten">
-                    <div className="p-grid">
-                        <div className="p-col-12">
-                            <FileInput label={"Tabellendaten hinzufügen..."} filesSelected={this.onSelectTabledataFiles} disabled={false}/>
-                        </div>
-                        {tablesfiles}
-                    </div>
-                </Panel>
-            </div>
-        );
-    }
+	public render(): JSX.Element
+	{
+		let geoFieldOptions;
+		if (this.props.geodata)
+		{
+			geoFieldOptions = R.map((field) => { return { label: field, value: field }; }, this.props.geodata.fields());
+		}
+		let geodatafile = <div className="p-col-12"/>;
+		if (this.state.geodatafile )
+		{
+			geodatafile = <div className="p-col-8">{this.state.geodatafile}</div>;
+		}
+		const tablesfiles = R.map((tablefile) => { return (<div key={tablefile} className="p-col-12">{tablefile}</div>); }, this.state.tablefiles);
+		return (
+			<div>
+				<Panel header="1. Geodaten">
+					<div className="p-grid p-justify-around">
+						<div className="p-col-12">
+							<FileInput label="Shape Datei auswählen..." filesSelected={this.onSelectGeodataFile} disabled={false}/>
+						</div>
+						{geodatafile}
+						<Dropdown className="p-col-6" key="geoId" value={this.state.geoId} options={geoFieldOptions} disabled={this.props.geodata == null} placeholder="ID Spalte auswählen" onChange={(e) => {this.setState({geoId: e.value}); } }/>
+						<Dropdown className="p-col-6" key="geoName" value={this.state.geoName} options={geoFieldOptions} disabled={this.props.geodata == null} placeholder="Namenspalte auswählen" onChange={(e) => {this.setState({geoName: e.value}); } }/>
+					</div>
+				</Panel>
+				<Panel header="2. Tabellendaten">
+					<div className="p-grid">
+						<div className="p-col-12">
+							<FileInput label={"Tabellendaten hinzufügen..."} filesSelected={this.onSelectTabledataFiles} disabled={false}/>
+						</div>
+						{tablesfiles}
+					</div>
+				</Panel>
+			</div>
+		);
+	}
 
-    private onSelectGeodataFile(files: FileList) {
-        Geodata.read(files[0].path, (newGeodata) => {
-            this.props.setGeodata(newGeodata.transformToWGS84());
-            this.setState({ geodatafile: files[0].path });
-        });
-    }
+	private onSelectGeodataFile(files: FileList)
+	{
+		Geodata.read(files[0].path, (newGeodata) => {
+			this.props.setGeodata(newGeodata.transformToWGS84());
+			this.setState({ geodatafile: files[0].path });
+		});
+	}
 
+	private onSelectTabledataFiles(files: FileList)
+	{
+		let newTablefiles = [] as string[];
+		for (let i=0;i<files.length; i++)
+		{
+			const start = files[i].path.length - 8;
+			const stop = start + 4;
+			const year = files[i].path.substring(start, stop);
+			this.addTabledataToDB(year, files[i].path);
+			newTablefiles = R.append(files[i].path, newTablefiles);
+		}
+		this.setState({ tablefiles: R.concat(newTablefiles, this.state.tablefiles) });
+	}
 
-    private onSelectTabledataFiles(files: FileList) {
-        let newTablefiles = [] as string[];
-        for (let i=0;i<files.length; i++) {
-            const start = files[i].path.length - 8;
-            const stop = start + 4;
-            const year = files[i].path.substring(start, stop);
-            this.addTabledataToDB(year, files[i].path);
-            newTablefiles = R.append(files[i].path, newTablefiles);
-        }
-        this.setState({ tablefiles: R.concat(newTablefiles, this.state.tablefiles) });
-    }
+	private getNameForId(id: string): string
+	{
+		if (!this.props.geodata)
+		{
+			return id;
+		}
+		const geoId = parseInt(id, 10) % 100;
+		const feature = this.props.geodata.getFeatureByFieldValue(this.state.geoId || "OT", `${geoId < 10 ? "0" + geoId : geoId}`);
+		if ( feature && feature.properties && this.state.geoName)
+		{
+			return feature.properties[this.state.geoName];
+		}
+		else
+		{
+			return id;
+		}
+	}
 
-    private getNameForId(id: string): string {
-        if (!this.props.geodata) {
-            return id;
-        }
-        const geoId = parseInt(id, 10) % 100;
-        const feature = this.props.geodata.getFeatureByFieldValue(this.state.geoId || "OT", `${geoId < 10 ? "0" + geoId : geoId}`);
-        if ( feature && feature.properties && this.state.geoName) {
-            return feature.properties[this.state.geoName];
-        } else {
-            return id;
-        }
-    }
+	private addTabledataToDB(year: string, path: string)
+	{
+		Tabledata.read(path, (tabledata) => {
+			const columnHeaders = R.slice(2, tabledata.getColumnCount() - 1, tabledata.getRowAt(3));
+			const rowHeaders = R.slice(4, tabledata.getRowCount() - 2, tabledata.getColumnAt(1));
+			const columnNames = R.map(this.getNameForId.bind(this), columnHeaders);
+			const rowNames = R.map(this.getNameForId.bind(this), rowHeaders);
+			const valueMatrix = tabledata.getTabledataBy([4, tabledata.getRowCount() - 2], [2, tabledata.getColumnCount()  - 1]);
+			for (let row = 0; row < valueMatrix.getRowCount(); row++)
+			{
+				for (let column = 0; column < valueMatrix.getColumnCount(); column++)
+				{
+					const wert = parseInt(valueMatrix.getValueAt(row, column), 10);
+					const nach = columnNames[column];
+					const von = rowNames[row];
+					const jahr = `${year}`;
+					this.props.db(`INSERT INTO matrices ('${nach}','${von}','${jahr}', ${isNaN(wert) ? "NULL" : wert});`);
+				}
+			}
+			this.props.addYear(year);
+		});
+	}
 
-    private addTabledataToDB(year: string, path: string) {
-        Tabledata.read(path, (tabledata) => {
-            const columnHeaders = R.slice(2, tabledata.getColumnCount() - 1, tabledata.getRowAt(3));
-            const rowHeaders = R.slice(4, tabledata.getRowCount() - 2, tabledata.getColumnAt(1));
-            const columnNames = R.map(this.getNameForId.bind(this), columnHeaders);
-            const rowNames = R.map(this.getNameForId.bind(this), rowHeaders);
-            const valueMatrix = tabledata.getTabledataBy([4, tabledata.getRowCount() - 2], [2, tabledata.getColumnCount()  - 1]);
-            for (let row = 0; row < valueMatrix.getRowCount(); row++) {
-                for (let column = 0; column < valueMatrix.getColumnCount(); column++) {
-                    const wert = parseInt(valueMatrix.getValueAt(row, column), 10);
-                    const nach = columnNames[column];
-                    const von = rowNames[row];
-                    const jahr = `${year}`;
-                    this.props.db(`INSERT INTO matrices ('${nach}','${von}','${jahr}', ${isNaN(wert) ? "NULL" : wert});`);
-                }
-            }
-            this.props.addYear(year);
-        });
-    }
 }
--- src/view/input/FileInput.tsx
+++ src/view/input/FileInput.tsx
@@ -1,30 +1,32 @@
 import {FileUpload} from "primereact/fileupload";
 import React from "react";
 
-export interface IFileInputProps {
-   label: string;
-   filesSelected: (files: FileList) => void;
-   disabled: boolean;
+export interface IFileInputProps
+{
+	label: string;
+	filesSelected: (files: FileList) => void;
+	disabled: boolean;
 }
 
-export default class FileInput extends React.Component<IFileInputProps>{
+export default class FileInput extends React.Component<IFileInputProps>
+{
 
-    constructor(props: IFileInputProps) {
-        super(props);
-        this.handleFiles = this.handleFiles.bind(this);
-    }
+	constructor(props: IFileInputProps)
+	{
+		super(props);
+		this.handleFiles = this.handleFiles.bind(this);
+	}
 
-    public render(): JSX.Element {
-       return (
-            <FileUpload mode="basic"
-                        disabled={this.props.disabled}
-                        chooseLabel={this.props.label}
-                        multiple={true}
-                        onSelect={this.handleFiles}/>
-       );
-    }
+	public render(): JSX.Element
+	{
+		return (
+			<FileUpload mode="basic" disabled={this.props.disabled} chooseLabel={this.props.label} multiple={true} onSelect={this.handleFiles}/>
+		);
+	}
 
-    private handleFiles(event: { originalEvent: Event, files: any}) {
-        this.props.filesSelected(event.files);
-    }
+	private handleFiles(event: { originalEvent: Event, files: any})
+	{
+		this.props.filesSelected(event.files);
+	}
+
 }
