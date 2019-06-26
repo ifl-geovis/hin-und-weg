5b051bb42e11eb700da91bc98df19ff03f2eab6b
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-06-26 08:40:53 +0000

    cleanup
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@178 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/Years.tsx
+++ src/view/Years.tsx
@@ -3,50 +3,58 @@ import { Panel } from "primereact/panel";
 import R from "ramda";
 import * as React from "react";
 
-export interface IYearsProps {
-    availableYears: string[];
-    selected: string[];
-    setYears: (newYears: string[]) => void;
+export interface IYearsProps
+{
+	availableYears: string[];
+	selected: string[];
+	setYears: (newYears: string[]) => void;
 }
 
-export default class Years extends React.Component<IYearsProps> {
+export default class Years extends React.Component<IYearsProps>
+{
 
-    constructor(props: IYearsProps) {
-        super(props);
-        this.onYearsChange = this.onYearsChange.bind(this);
-        this.makeCheckBox = this.makeCheckBox.bind(this);
-    }
+	constructor(props: IYearsProps)
+	{
+		super(props);
+		this.onYearsChange = this.onYearsChange.bind(this);
+		this.makeCheckBox = this.makeCheckBox.bind(this);
+	}
 
-    public render(): JSX.Element {
-        const checkboxes = R.map(this.makeCheckBox, this.props.availableYears.sort());
-        return (
-            <Panel header="Jahr(e)">
-                <div className="p-grid" style={{ margin: "10px" }}>
-                    {checkboxes}
-                </div>
-            </Panel>
-        );
-    }
+	public render(): JSX.Element
+	{
+		const checkboxes = R.map(this.makeCheckBox, this.props.availableYears.sort());
+		return (
+			<Panel header="Jahr(e)">
+				<div className="p-grid" style={{ margin: "10px" }}>
+					{checkboxes}
+				</div>
+			</Panel>
+		);
+	}
 
-    private makeCheckBox(year: string){
-        return (
-            <div key={year} className="p-col-12">
-                <Checkbox inputId={year} value={year}
-                          onChange={this.onYearsChange} checked={R.includes(year, this.props.selected)}></Checkbox>
-                <label htmlFor="cb-{year}" className="p-checkbox-label">{year}</label>
-            </div>
-        );
-    }
+	private makeCheckBox(year: string)
+	{
+		return (
+			<div key={year} className="p-col-12">
+				<Checkbox inputId={year} value={year} onChange={this.onYearsChange} checked={R.includes(year, this.props.selected)}></Checkbox>
+				<label htmlFor="cb-{year}" className="p-checkbox-label">{year}</label>
+			</div>
+		);
+	}
 
-    private onYearsChange(e: { originalEvent: Event, value: string, checked: boolean}) {
-        let selectedYears = this.props.selected;
-        const selectedYear = e.value;
-        if (e.checked) {
-            selectedYears = R.append(selectedYear, selectedYears);
-        } else {
-            selectedYears = R.reject(R.equals(selectedYear), selectedYears);
-        }
-        this.props.setYears(R.uniq(selectedYears));
-    }
+	private onYearsChange(e: { originalEvent: Event, value: string, checked: boolean})
+	{
+		let selectedYears = this.props.selected;
+		const selectedYear = e.value;
+		if (e.checked)
+		{
+			selectedYears = R.append(selectedYear, selectedYears);
+		}
+		else
+		{
+			selectedYears = R.reject(R.equals(selectedYear), selectedYears);
+		}
+		this.props.setYears(R.uniq(selectedYears));
+	}
 
 }
