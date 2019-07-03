452a188fa92ca86c1abe70aa26807387c7264fb0
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2019-07-03 14:39:08 +0000

    packaging
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@193 366f23da-3ab8-43e7-bb03-06f30a681aac

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo cleanup all distribution artifacts
 rm -rf hin-und-weg-*
+rm -rf dist
 rm -f *.tgz
 rm -f *.zip
--- pack.sh
+++ pack.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-VERSION=1.0.0
+VERSION=1.1.0
+./clean.sh
+npm run build
 electron-packager --platform=win32,darwin,linux .
 zip -r "hin&weg-linux-$VERSION.zip" hin-und-weg-linux-x64/
 zip -r "hin&weg-windows-$VERSION.zip" hin-und-weg-win32-x64/
