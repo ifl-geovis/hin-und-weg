12556a1d65f0dc8e8946f976c5599399fa4a7d17
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-01-10 16:43:06 +0000

    log
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@239 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/main.ts
+++ src/main.ts
@@ -64,4 +64,4 @@ app.on("activate", () =>
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
-Log.debug("Started Hin & weg application ");
+Log.debug("Started hin&weg application ");
