80d55d44b436c45b154881ba2c729ec50990182e
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-04-13 11:52:10 +0000

    logic to make dashboard-views switchable
    git-svn-id: svn://svn.geoway.de/hin-und-weg/branches/dashboards@292 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/dashboards/DashboardView.tsx
+++ src/view/dashboards/DashboardView.tsx
@@ -49,21 +49,54 @@ export default class DashboardView extends React.Component<IDashboardProps, IDas
 		super(props);
 		this.state =
 		{
-			dashboard_configuration: "single",
+			dashboard_configuration: "s1",
 		};
 	}
 
 	public render(): JSX.Element
 	{
-		//let views = this.getVisibleViews();
-		//let showedView = this.selectCurrentView(this.state.activeView);
+		return this.selectCurrentView(this.state.dashboard_configuration);
+	}
+
+	private getViewSwitcher(): JSX.Element
+	{
+		return (
+			<ViewSwitcher geodata={this.props.geodata} db={this.props.db} items={this.props.items} timeline={this.props.timeline} geoName={this.props.geoName} geoId={this.props.geoId} locations={this.props.locations} location={this.props.location} theme={this.props.theme} yearsAvailable={this.props.yearsAvailable} onSelectLocation={this.props.onSelectLocation} setGeodata={this.props.setGeodata} setGeoName={this.props.setGeoName} setGeoId={this.props.setGeoId} addYear={this.props.addYear} />
+		);
+	}
+
+	private selectCurrentView(view: string): JSX.Element
+	{
+		if (view == "t1b1") return this.select_t1b1();
+		if (view == "l1r1") return this.select_l1r1();
+		return this.getViewSwitcher();
+	}
+
+	private select_t1b1(): JSX.Element
+	{
+		let viewswitcher = this.getViewSwitcher();
+		return (
+			<div className="p-grid">
+				<div className="p-col-12">
+					{viewswitcher}
+				</div>
+				<div className="p-col-12">
+					{viewswitcher}
+				</div>
+			</div>
+		);
+	}
+
+	private select_l1r1(): JSX.Element
+	{
+		let viewswitcher = this.getViewSwitcher();
 		return (
 			<div className="p-grid">
 				<div className="p-col-6">
-					<ViewSwitcher geodata={this.props.geodata} db={this.props.db} items={this.props.items} timeline={this.props.timeline} geoName={this.props.geoName} geoId={this.props.geoId} locations={this.props.locations} location={this.props.location} theme={this.props.theme} yearsAvailable={this.props.yearsAvailable} onSelectLocation={this.props.onSelectLocation} setGeodata={this.props.setGeodata} setGeoName={this.props.setGeoName} setGeoId={this.props.setGeoId} addYear={this.props.addYear} />
+					{viewswitcher}
 				</div>
 				<div className="p-col-6">
-					<ViewSwitcher geodata={this.props.geodata} db={this.props.db} items={this.props.items} timeline={this.props.timeline} geoName={this.props.geoName} geoId={this.props.geoId} locations={this.props.locations} location={this.props.location} theme={this.props.theme} yearsAvailable={this.props.yearsAvailable} onSelectLocation={this.props.onSelectLocation} setGeodata={this.props.setGeodata} setGeoName={this.props.setGeoName} setGeoId={this.props.setGeoId} addYear={this.props.addYear} />
+					{viewswitcher}
 				</div>
 			</div>
 		);
