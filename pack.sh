50f92fb28aae9017b08749dd63c2626bd81d7fd0
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-04-15 17:45:06 +0000

    update version number
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@302 366f23da-3ab8-43e7-bb03-06f30a681aac

--- pack.sh
+++ pack.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-VERSION=1.1.0
+VERSION=1.2.0
 ./clean.sh
 npm run build
 electron-packager --platform=win32,darwin,linux .
-zip -r "hin&weg-linux-$VERSION.zip" hin-und-weg-linux-x64/
-zip -r "hin&weg-windows-$VERSION.zip" hin-und-weg-win32-x64/
-zip -r "hin&weg-mac-$VERSION.zip" hin-und-weg-darwin-x64/
+zip -r "hin&weg-$VERSION-linux.zip" hin-und-weg-linux-x64/
+zip -r "hin&weg-$VERSION-windows.zip" hin-und-weg-win32-x64/
+zip -r "hin&weg-$VERSION-mac.zip" hin-und-weg-darwin-x64/
