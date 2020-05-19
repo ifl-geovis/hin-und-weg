185ae4e2c8fc9d6152b2a592482c53f5ffba7e56
pelle <pelle@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-05-19 11:42:01 +0000

    Alle änderungen, Map ist noch falsch
    git-svn-id: svn://svn.geoway.de/hin-und-weg/branches/NeueKarte@318 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/geo/GeodataView.tsx
+++ src/view/geo/GeodataView.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Geodata from "../../model/Geodata";
 import SelectInput from "../input/SelectInput";
 import MapView from "./MapView";
+import NeueMapView from "./NeueMapView";
 import { Checkbox } from "primereact/checkbox";
 
 export interface IGeodataProps
@@ -44,7 +45,8 @@ export default class GeodataView extends React.Component<IGeodataProps, IGeodata
 					<label htmlFor="showlabels" className="p-checkbox-label">zeige Namen</label>
 				</div>
 				<div className="p-col-12">
-					<MapView geodata={this.props.geodata} nameField={this.props.geoName} items={this.props.items} selectedLocation={this.props.selectedLocation} onSelectLocation={this.props.onSelectLocation} showLabels={this.state.showLabels} theme={this.props.theme}/>
+					{/* <MapView geodata={this.props.geodata} nameField={this.props.geoName} items={this.props.items} selectedLocation={this.props.selectedLocation} onSelectLocation={this.props.onSelectLocation} showLabels={this.state.showLabels} theme={this.props.theme}/> */}
+					<NeueMapView geodata={this.props.geodata} nameField={this.props.geoName} items={this.props.items} selectedLocation={this.props.selectedLocation} onSelectLocation={this.props.onSelectLocation} showLabels={this.state.showLabels} theme={this.props.theme}/>
 				</div>
 
 				<div className="p-col-1" > <svg> <rect key="area" fill="#cbf719" stroke="#4d4d4d" width="24" height="24" margin-right="15"></rect> </svg> </div>
