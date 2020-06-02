99ff648291a127c940ad8458be4904559fc00f00
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-06-02 10:49:21 +0000

    remove amcharts
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@363 366f23da-3ab8-43e7-bb03-06f30a681aac

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env sh
 npm install
 cp -r extra/types/*  node_modules/@types/
-cp extra/amcharts/charts.js node_modules/@amcharts/
-cp extra/amcharts/core.js node_modules/@amcharts/
-cp extra/amcharts/animated.js node_modules/@amcharts/
