a8e170bba70a523f6bda712b7c8f0dbf9a26a2b2
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-06-11 08:09:37 +0000

    remove svn-dirs in distribution
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@398 366f23da-3ab8-43e7-bb03-06f30a681aac

--- pack.sh
+++ pack.sh
@@ -3,6 +3,9 @@ VERSION=1.3.2
 ./clean.sh
 npm run build
 electron-packager --platform=win32,darwin,linux .
+rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/.svn
+rm -rf hin-und-weg-win32-x64/resources/app/.svn
+rm -rf hin-und-weg-linux-x64/resources/app/.svn
 zip -r "hin&weg-$VERSION-linux.zip" hin-und-weg-linux-x64/
 zip -r "hin&weg-$VERSION-windows.zip" hin-und-weg-win32-x64/
 zip -r "hin&weg-$VERSION-mac.zip" hin-und-weg-darwin-x64/
