a748e4ca5a4e6dd41c8bfd6d7a514710180646c2
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-10-29 11:26:02 +0000

    #2799: add saveable settings
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@510 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/settings.ts
+++ src/settings.ts
@@ -1,21 +1,44 @@
 import fs from 'fs';
+import os from 'os';
 
 import Config from "./config";
 import Log from './log';
 
-export default class Settings
-{
+export default class Settings {
 
-	private static savepath: string = '.hin&weg';
+	private static savepath: string = os.homedir() + '/.hin&weg';
 	private static settings: any = JSON.parse('{}');
 
-	public static load()
-	{
+	public static load() {
 		try {
 			Settings.settings = JSON.parse(fs.readFileSync(Settings.savepath, 'utf8'));
 		} catch (e) {
-			Log.debug("Couldnot load file " + Settings.savepath + ": " + e);
+			Log.debug("Could not load file " + Settings.savepath + ": " + e);
 		}
+	}
+
+	public static save() {
+		try {
+			fs.writeFileSync(Settings.savepath, JSON.stringify(Settings.settings), 'utf8');
+		} catch (e) {
+			Log.debug("Could not write file " + Settings.savepath + ": " + e);
+		}
+	}
+
+	private static getSection(section: string): any {
+		if (section in Settings.settings) return Settings.settings[section];
+		return null;
+	}
+
+	public static getValue(section: string, key: string): any {
+		const sec = Settings.getSection(section);
+		if ((sec != null) && (key in sec)) return sec[key];
+		return Config.getValue(section, key);
+	}
+
+	public static setValue(section: string, key: string, value: any) {
+		if (Settings.getSection(section) == null) Settings.settings[section] = {};
+		Settings.settings[section][key] = value;
 	}
 
 }
