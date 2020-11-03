daece7994d075ae580f798ec48a3e3565eb26cd2
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-11-03 16:05:51 +0000

    #4913: creating settings
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@533 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/components/SettingsView.tsx
+++ src/view/components/SettingsView.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { TabPanel, TabView } from 'primereact/tabview';
 import { Dropdown } from 'primereact/dropdown';
 import { InputText } from 'primereact/inputtext';
+import {ColorPicker} from 'primereact/colorpicker';
 import { Button } from 'primereact/button';
+
 import Settings from '../../settings';
 import OfflineMaps from '../../data/OfflineMaps';
 
@@ -13,6 +15,7 @@ interface ISettingsState {
 }
 
 export default class SettingsView extends React.Component<ISettingsProps, ISettingsState> {
+
 	private legendPlacementSelections = [
 		{ label: 'Unten', value: 'bottom' },
 		{ label: 'Oben', value: 'top' },
@@ -28,41 +31,15 @@ export default class SettingsView extends React.Component<ISettingsProps, ISetti
 	}
 
 	public render(): JSX.Element {
-		// const test1 = this.getTest1();
-		// const test2 = this.getTest2();
 		const map = this.getMapSettings();
+		const colorschemes = this.getColorSchemes();
 		return (
 			<TabView className="p-tabview-right" activeIndex={0}>
 				<TabPanel header="Karte">{map}</TabPanel>
+				<TabPanel header="Farbschemata">{colorschemes}</TabPanel>
 			</TabView>
 		);
 	}
-
-	// private getTest1() {
-	// 	const dropdowninput1 = this.createDropdownInput('Dropdowninput 1 für Test 1', 'test1', 'dropdowninput1', this.test1selections);
-	// 	const textinput1 = this.createTextInput('Textinput 1 für Test 1', 'test1', 'textinput1');
-	// 	const textinput2 = this.createTextInput('Textinput 2 für Test 1', 'test1', 'textinput2');
-	// 	return (
-	// 		<div className="p-grid">
-	// 			<div className="p-col-12">
-	// 				{dropdowninput1}
-	// 				{textinput1}
-	// 				{textinput2}
-	// 				<Button label="Speichern" onClick={this.saveSettings} />
-	// 			</div>
-	// 		</div>
-	// 	);
-	// }
-
-	// private getTest2() {
-	// 	return (
-	// 		<div className="p-grid">
-	// 			<div className="p-col-12">
-	// 				<h1>Test 2</h1>
-	// 			</div>
-	// 		</div>
-	// 	);
-	// }
 
 	private getMapSettings() {
 		const dropdownLegendPlacement = this.createDropdownInput(
@@ -97,6 +74,46 @@ export default class SettingsView extends React.Component<ISettingsProps, ISetti
 				</p>
 				{dropdownLegendPlacement}
 				<Button label="Speichern" onClick={this.saveSettings} style={{ marginTop: '2em' }} />
+			</div>
+		);
+	}
+
+	private getColorSchemes() {
+		const scheme1 = this.getColorSchemePickers('scheme1');
+		const scheme2 = this.getColorSchemePickers('scheme2');
+		return (
+			<div>
+				<h1>benutzerdefinierte Farbschemata</h1>
+				<h2>Schema 1</h2>
+				{scheme1}
+				<h2>Schema 2</h2>
+				{scheme2}
+				<Button label="Speichern" onClick={this.saveSettings} style={{ marginTop: '2em' }} />
+			</div>
+		);
+	}
+
+	private getColorSchemePickers(basename: string) {
+		const color1 = this.createColorPicker('user-color-schemes', basename +'-color1');
+		const color2 = this.createColorPicker('user-color-schemes', basename +'-color2');
+		const color3 = this.createColorPicker('user-color-schemes', basename +'-color3');
+		const color4 = this.createColorPicker('user-color-schemes', basename +'-color4');
+		const color5 = this.createColorPicker('user-color-schemes', basename +'-color5');
+		const color6 = this.createColorPicker('user-color-schemes', basename +'-color6');
+		const color7 = this.createColorPicker('user-color-schemes', basename +'-color7');
+		const color8 = this.createColorPicker('user-color-schemes', basename +'-color8');
+		const color9 = this.createColorPicker('user-color-schemes', basename +'-color9');
+		return (
+			<div>
+				{color1}
+				{color2}
+				{color3}
+				{color4}
+				{color5}
+				{color6}
+				{color7}
+				{color8}
+				{color9}
 			</div>
 		);
 	}
@@ -138,8 +155,14 @@ export default class SettingsView extends React.Component<ISettingsProps, ISetti
 		);
 	}
 
-	private processTest1(event: { originalEvent: Event; value: any }) {
-		//this.props.setAlgorithm(event.value.value);
+	private createColorPicker(section: string, key: string) {
+		return (
+			<ColorPicker
+				id={'colorpicker-' + section + '-' + key}
+				value={Settings.getValue(section, key)}
+				onChange={(e) => this.processInput(section, key, e)}
+			/>
+		);
 	}
 
 	private processInput(section: string, key: string, event: any) {
