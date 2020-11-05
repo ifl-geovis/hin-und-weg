eaa8cfd211c4950bf303fb1436e9aace58a91e93
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-11-05 15:32:36 +0000

    #5710: setting custom class borders
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@554 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/components/SettingsView.tsx
+++ src/view/components/SettingsView.tsx
@@ -3,6 +3,7 @@ import { TabPanel, TabView } from 'primereact/tabview';
 import { Dropdown } from 'primereact/dropdown';
 import { InputText } from 'primereact/inputtext';
 import { ColorPicker } from 'primereact/colorpicker';
+import { Slider } from 'primereact/slider';
 import { Button } from 'primereact/button';
 
 import Settings from '../../settings';
@@ -15,11 +16,14 @@ interface ISettingsState {
 }
 
 export default class SettingsView extends React.Component<ISettingsProps, ISettingsState> {
+
 	private legendPlacementSelections = [
 		{ label: 'Unter der Karte', value: 'bottom' },
 		{ label: 'Über der Karte', value: 'top' },
 	];
 	private colorSchemeDefault = ['cc8844', 'bb8855', 'aa8866', '998877', '888888', '778899', '6688aa', '5588bb', '4488cc'];
+	private classificationPositiveDefault = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	private classificationNegativeDefault = [-1, -2, -3, -4, -5, -6, -7, -8, -9];
 
 	constructor(props: any, state: any) {
 		super(props);
@@ -34,10 +38,12 @@ export default class SettingsView extends React.Component<ISettingsProps, ISetti
 	public render(): JSX.Element {
 		const map = this.getMapSettings();
 		const colorschemes = this.getColorSchemes();
+		const classification = this.getClassification();
 		return (
 			<TabView className="p-tabview-right" activeIndex={0}>
 				<TabPanel header="Karte">{map}</TabPanel>
-				<TabPanel header="Farbschemata">{colorschemes}</TabPanel>
+				<TabPanel header="Farben">{colorschemes}</TabPanel>
+				<TabPanel header="Klassen">{classification}</TabPanel>
 			</TabView>
 		);
 	}
@@ -129,6 +135,51 @@ export default class SettingsView extends React.Component<ISettingsProps, ISetti
 		);
 	}
 
+	private getClassification() {
+		const inputpositive1 = this.createClassificationInput('classification', 'positive', 0, true);
+		const inputpositive2 = this.createClassificationInput('classification', 'positive', 1, true);
+		const inputpositive3 = this.createClassificationInput('classification', 'positive', 2, true);
+		const inputpositive4 = this.createClassificationInput('classification', 'positive', 3, true);
+		const inputpositive5 = this.createClassificationInput('classification', 'positive', 4, true);
+		const inputpositive6 = this.createClassificationInput('classification', 'positive', 5, true);
+		const inputpositive7 = this.createClassificationInput('classification', 'positive', 6, true);
+		const inputpositive8 = this.createClassificationInput('classification', 'positive', 7, true);
+		const inputnegative1 = this.createClassificationInput('classification', 'negative', 0, false);
+		const inputnegative2 = this.createClassificationInput('classification', 'negative', 1, false);
+		const inputnegative3 = this.createClassificationInput('classification', 'negative', 2, false);
+		const inputnegative4 = this.createClassificationInput('classification', 'negative', 3, false);
+		const inputnegative5 = this.createClassificationInput('classification', 'negative', 4, false);
+		const inputnegative6 = this.createClassificationInput('classification', 'negative', 5, false);
+		const inputnegative7 = this.createClassificationInput('classification', 'negative', 6, false);
+		const inputnegative8 = this.createClassificationInput('classification', 'negative', 7, false);
+		return (
+			<div>
+				<h1>benutzerdefinierte Klassengrenzen</h1>
+				<h2>positive Werte</h2>
+				{inputpositive1}
+				{inputpositive2}
+				{inputpositive3}
+				{inputpositive4}
+				{inputpositive5}
+				{inputpositive6}
+				{inputpositive7}
+				{inputpositive8}
+				<h2>negative Werte</h2>
+				{inputnegative1}
+				{inputnegative2}
+				{inputnegative3}
+				{inputnegative4}
+				{inputnegative5}
+				{inputnegative6}
+				{inputnegative7}
+				{inputnegative8}
+				<div>
+					<Button label="Speichern" onClick={this.saveSettings} style={{ marginTop: '2em' }} />
+				</div>
+			</div>
+		);
+	}
+
 	selectOfflinePath(files: any) {
 		const name = files[0].name;
 		const path = files[0].path.slice(0, -name.length);
@@ -180,6 +231,28 @@ export default class SettingsView extends React.Component<ISettingsProps, ISetti
 		);
 	}
 
+	private createClassificationInput(section: string, key: string, index: number, positive: boolean) {
+		let classification = Settings.getValue(section, key);
+		const defs = (positive) ? this.classificationPositiveDefault : this.classificationNegativeDefault
+		if (classification == null) classification = defs;
+		let value = classification[index];
+		return (
+			<InputText
+				id={'classification-' + section + '-' + key + '-' + index}
+				value={value}
+				onChange={(e) => this.processClassificationInput(section, key, e, index, defs)}
+			/>
+		);
+	}
+
+	private processInput(section: string, key: string, event: any) {
+		// @ts-ignore
+		const value = event.target.value;
+		Settings.setValue(section, key, value);
+		Settings.save();
+		this.setState({ change: this.state.change ? false : true });
+	}
+
 	private processColorInput(section: string, key: string, event: any, index: number) {
 		// @ts-ignore
 		const value = event.target.value;
@@ -191,10 +264,13 @@ export default class SettingsView extends React.Component<ISettingsProps, ISetti
 		this.setState({ change: this.state.change ? false : true });
 	}
 
-	private processInput(section: string, key: string, event: any) {
+	private processClassificationInput(section: string, key: string, event: any, index: number, defs: number[]) {
 		// @ts-ignore
-		const value = event.target.value;
-		Settings.setValue(section, key, value);
+		let value = event.currentTarget.value;
+		let classification = Settings.getValue(section, key);
+		if (classification == null) classification = defs;
+		classification[index] = value;
+		Settings.setValue(section, key, classification);
 		Settings.save();
 		this.setState({ change: this.state.change ? false : true });
 	}
