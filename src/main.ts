b41d4df46428a6169a4cc11e9992dd651c167d88
shorn <shorn@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-11-18 15:22:07 +0000

    update electron to Version 5.X for fixing bug for PDF printing on windows;
    git-svn-id: svn://svn.geoway.de/hin-und-weg/branches/latestElectron@575 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,63 @@
-import { app, BrowserWindow, Menu } from "electron";
-import path from "path";
-import Log from "./log";
-import Config from "./config";
-import MainMenu from "./menu";
+import { app, BrowserWindow, Menu } from 'electron';
+import path from 'path';
+import Log from './log';
+import Config from './config';
+import MainMenu from './menu';
 
 let mainWindow: Electron.BrowserWindow;
-
-function createWindow()
-{
+function createWindow() {
 	// Create the browser window.
-	mainWindow = new BrowserWindow(
-		{
-			height: 960,
-			width: 1400,
-			// tslint:disable-next-line: object-literal-sort-keys
-			title: "hin&weg " + Config.getVersion(),
-		});
+	mainWindow = new BrowserWindow({
+		height: 960,
+		width: 1400,
+		// tslint:disable-next-line: object-literal-sort-keys
+		title: 'hin&weg ' + Config.getVersion(),
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
 	// and load the index.html of the app.
-	mainWindow.loadFile(path.join(__dirname, "../index.html"));
+	mainWindow.loadFile(path.join(__dirname, '../index.html'));
 
 	// set main menu
 	Menu.setApplicationMenu(MainMenu.getMainMenu());
 
 	// Open the DevTools.
-	if (Config.getValue("global", "devtools"))
-	{
-		mainWindow.webContents.openDevTools({mode: "undocked"});
+	if (Config.getValue('global', 'devtools')) {
+		mainWindow.webContents.openDevTools({ mode: 'undocked' });
 	}
 
 	// Emitted when the window is closed.
-	mainWindow.on("closed", () =>
-		{
-			// Dereference the window object, usually you would store windows
-			// in an array if your app supports multi windows, this is the time
-			// when you should delete the corresponding element.
-		});
+	mainWindow.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+	});
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () =>
-	{
-		// On OS X it is common for applications and their menu bar
-		// to stay active until the user quits explicitly with Cmd + Q
-		//if (process.platform !== "darwin") {
-		app.quit();
-		//}
-	});
+app.on('window-all-closed', () => {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	//if (process.platform !== "darwin") {
+	app.quit();
+	//}
+});
 
-app.on("activate", () =>
-	{
-		// On OS X it"s common to re-create a window in the app when the
-		// dock icon is clicked and there are no other windows open.
-		if (mainWindow === null)
-		{
-			createWindow();
-		}
-	});
+app.on('activate', () => {
+	// On OS X it"s common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
-Log.debug("Started hin&weg application ");
+Log.debug('Started hin&weg application ');
