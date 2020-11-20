88c07ed5752048a791c748f8652237adcb8cc527
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-11-20 15:35:09 +0000

    style
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@581 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/Location.tsx
+++ src/view/Location.tsx
@@ -5,29 +5,29 @@ import SelectInput from "./input/SelectInput";
 
 export interface ILocationProps
 {
-  title: string;
-  locations: string[];
-  selectedLocation: string | null;
-  onSelectLocation: (newLocation: string) => void;
+	title: string;
+	locations: string[];
+	selectedLocation: string | null;
+	onSelectLocation: (newLocation: string) => void;
 }
 
 export default class Location extends React.Component<ILocationProps>
 {
 
-  constructor(props: ILocationProps)
-  {
-    super(props);
-  }
+	constructor(props: ILocationProps)
+	{
+		super(props);
+	}
 
-  public render(): JSX.Element
-  {
-    return (
-      <Accordion activeIndex={0}>
-        <AccordionTab header={this.props.title}>
-          <SelectInput options={this.props.locations} selected={this.props.selectedLocation} onSelected={this.props.onSelectLocation}/>
-        </AccordionTab>
-      </Accordion>
-    );
-  }
+	public render(): JSX.Element
+	{
+		return (
+			<Accordion activeIndex={0}>
+				<AccordionTab header={this.props.title}>
+					<SelectInput options={this.props.locations} selected={this.props.selectedLocation} onSelected={this.props.onSelectLocation}/>
+				</AccordionTab>
+			</Accordion>
+		);
+	}
 
 }
