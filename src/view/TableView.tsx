b8e7a1637404d5d45244ef9493ba144059e22edf
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-11-23 17:04:30 +0000

    formatting
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@585 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/TableView.tsx
+++ src/view/TableView.tsx
@@ -7,48 +7,47 @@ import { systemPreferences } from "electron";
 export type TableItem  = {[name: string]: any} | null;
 
 export interface ITableViewProps {
-    items: TableItem[];
-    maxRows?: number | null;
+	items: TableItem[];
+	maxRows?: number | null;
 }
 
 export default class TableView extends React.Component<ITableViewProps> {
 
-    constructor(props: ITableViewProps) {
-        super(props);
-    }
+	constructor(props: ITableViewProps) {
+		super(props);
+	}
 
-    public render(): JSX.Element {
-        if (R.isEmpty(this.props.items)) {
-            return <div>Keine Daten vorhanden.</div>;
-        }
-        const fieldNames = R.keys(this.props.items[0]);
-        var name;
-        var id = 0
-        for(name of fieldNames){
-            if(name.valueOf() == "Absolutwert"){
-                fieldNames.splice(id,1);
-            }
-            id +=1;
-        } 
+	public render(): JSX.Element {
+		if (R.isEmpty(this.props.items)) {
+			return <div>Keine Daten vorhanden.</div>;
+		}
+		const fieldNames = R.keys(this.props.items[0]);
+		var name;
+		var id = 0
+		for(name of fieldNames){
+			if(name.valueOf() == "Absolutwert"){
+				fieldNames.splice(id,1);
+			}
+			id +=1;
+		}
+		const columns = R.map(this.createColumn, fieldNames);
+		return (
+			<DataTable value={this.props.items} paginator={true} rows={this.props.maxRows || 10}
+				paginatorTemplate="FirstPageLink PrevPageLink PageLinks CurrentPageReport NextPageLink LastPageLink"
+				currentPageReportTemplate="({currentPage} von {totalPages})"
+				emptyMessage="Keine Daten vorhanden.">
+				{columns}
+			</DataTable>
+		);
+	}
 
+	private createColumn(fieldName: string): JSX.Element {
+		let filterMatchMode = "contains";
+		if (fieldName === "Wert"){
+			filterMatchMode = "equals";
+		}
+		return <Column key={fieldName}  field={`${fieldName}`} header={fieldName} filterPlaceholder="Filtern ..."
+							sortable={true} filter={true} filterMatchMode={filterMatchMode}/>;
+	}
 
-        const columns = R.map(this.createColumn, fieldNames);
-        return (
-            <DataTable value={this.props.items} paginator={true} rows={this.props.maxRows || 10}
-                paginatorTemplate="FirstPageLink PrevPageLink PageLinks CurrentPageReport NextPageLink LastPageLink"
-                currentPageReportTemplate="({currentPage} von {totalPages})"
-                emptyMessage="Keine Daten vorhanden.">
-                {columns}
-            </DataTable>
-        );
-    }
-
-    private createColumn(fieldName: string): JSX.Element {
-        let filterMatchMode = "contains";
-        if (fieldName === "Wert"){
-            filterMatchMode = "equals";
-        }
-        return <Column key={fieldName}  field={`${fieldName}`} header={fieldName} filterPlaceholder="Filtern ..."
-                        sortable={true} filter={true} filterMatchMode={filterMatchMode}/>;
-    }
 }
