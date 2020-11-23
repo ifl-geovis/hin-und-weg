5c135974f94b6c78b411037933f4c0058f1e280d
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2020-11-23 17:05:16 +0000

    #4863: user defined alasql-aggregator
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@586 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/start.tsx
+++ src/start.tsx
@@ -6,6 +6,20 @@ import Settings from './settings';
 
 function setupDB() {
 	const DB = alasql;
+	DB.aggr.MYSUM = function(value, accumulator, stage) {
+	if(stage == 1) {
+		// first call of aggregator - for first line
+		var newAccumulator =  value;
+		return newAccumulator;
+	} else if(stage == 2) {
+		// for every line in the group
+		accumulator = accumulator + value;
+		return accumulator;
+	} else if(stage == 3) {
+		// Post production - please nota that value Will be undefined
+		return accumulator;
+	}
+}
 	DB('CREATE DATABASE hin_und_weg');
 	DB('USE hin_und_weg');
 	DB('CREATE TABLE matrices (Nach STRING,Von STRING ,Jahr STRING ,Wert FLOAT);');
