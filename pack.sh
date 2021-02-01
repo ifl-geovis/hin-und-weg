2513258440e17e6eb6df2aa9ed444680184f5c31
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2021-02-01 14:49:42 +0000

    #6111: remove old testdata and put new testdata at the base of the application
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@608 366f23da-3ab8-43e7-bb03-06f30a681aac

--- pack.sh
+++ pack.sh
@@ -2,10 +2,21 @@
 VERSION=1.5.3
 ./clean.sh
 npm run build
+# create installations
 electron-packager --platform=win32,darwin,linux .
+# cleanup mac installation
 rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/.svn
+rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/testdata/testdata-old/
+mv hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/testdata hin-und-weg-darwin-x64/
+# cleanup win installation
 rm -rf hin-und-weg-win32-x64/resources/app/.svn
+rm -rf hin-und-weg-win32-x64/resources/app/testdata/testdata-old/
+mv hin-und-weg-win32-x64/resources/app/testdata hin-und-weg-win32-x64/
+# cleanup linux installation
 rm -rf hin-und-weg-linux-x64/resources/app/.svn
+rm -rf hin-und-weg-linux-x64/resources/app/testdata/testdata-old/
+mv hin-und-weg-linux-x64/resources/app/testdata hin-und-weg-linux-x64/
+# package distributions
 zip -r "hin&weg-$VERSION-linux.zip" hin-und-weg-linux-x64/
 zip -r "hin&weg-$VERSION-windows.zip" hin-und-weg-win32-x64/
 zip -r "hin&weg-$VERSION-mac.zip" hin-und-weg-darwin-x64/
