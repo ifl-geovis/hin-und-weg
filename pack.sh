f7d8ab41c4150d72fd301bbe31f16bd8739518b7
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2021-02-01 16:13:12 +0000

    #5971: separate offline folder
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@611 366f23da-3ab8-43e7-bb03-06f30a681aac

--- pack.sh
+++ pack.sh
@@ -5,17 +5,20 @@ npm run build
 # create installations
 electron-packager --platform=win32,darwin,linux .
 # cleanup mac installation
-rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/.svn
+rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/.*
 rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/testdata/testdata-old/
 mv hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/testdata hin-und-weg-darwin-x64/
+mv hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/offline hin-und-weg-darwin-x64/
 # cleanup win installation
-rm -rf hin-und-weg-win32-x64/resources/app/.svn
+rm -rf hin-und-weg-win32-x64/resources/app/.*
 rm -rf hin-und-weg-win32-x64/resources/app/testdata/testdata-old/
 mv hin-und-weg-win32-x64/resources/app/testdata hin-und-weg-win32-x64/
+mv hin-und-weg-win32-x64/resources/app/offline hin-und-weg-win32-x64/
 # cleanup linux installation
-rm -rf hin-und-weg-linux-x64/resources/app/.svn
+rm -rf hin-und-weg-linux-x64/resources/app/.*
 rm -rf hin-und-weg-linux-x64/resources/app/testdata/testdata-old/
 mv hin-und-weg-linux-x64/resources/app/testdata hin-und-weg-linux-x64/
+mv hin-und-weg-linux-x64/resources/app/offline hin-und-weg-linux-x64/
 # package distributions
 zip -r "hin&weg-$VERSION-linux.zip" hin-und-weg-linux-x64/
 zip -r "hin&weg-$VERSION-windows.zip" hin-und-weg-win32-x64/
