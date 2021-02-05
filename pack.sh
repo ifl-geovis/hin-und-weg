5a8ac644811a976d89cc6ea4276f1247b0f52e4a
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2021-02-05 11:47:59 +0000

    #6111: use new data for distribution
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@619 366f23da-3ab8-43e7-bb03-06f30a681aac

--- pack.sh
+++ pack.sh
@@ -6,18 +6,18 @@ npm run build
 electron-packager --platform=win32,darwin,linux .
 # cleanup mac installation
 rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/.*
-rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/testdata/testdata-old/
-mv hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/testdata hin-und-weg-darwin-x64/
+rm -rf hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/testdata/
+mv hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/distdata hin-und-weg-darwin-x64/data
 mv hin-und-weg-darwin-x64/hin-und-weg.app/Contents/Resources/app/offline hin-und-weg-darwin-x64/
 # cleanup win installation
 rm -rf hin-und-weg-win32-x64/resources/app/.*
-rm -rf hin-und-weg-win32-x64/resources/app/testdata/testdata-old/
-mv hin-und-weg-win32-x64/resources/app/testdata hin-und-weg-win32-x64/
+rm -rf hin-und-weg-win32-x64/resources/app/testdata/
+mv hin-und-weg-win32-x64/resources/app/distdata hin-und-weg-win32-x64/data
 mv hin-und-weg-win32-x64/resources/app/offline hin-und-weg-win32-x64/
 # cleanup linux installation
 rm -rf hin-und-weg-linux-x64/resources/app/.*
-rm -rf hin-und-weg-linux-x64/resources/app/testdata/testdata-old/
-mv hin-und-weg-linux-x64/resources/app/testdata hin-und-weg-linux-x64/
+rm -rf hin-und-weg-linux-x64/resources/app/testdata/
+mv hin-und-weg-linux-x64/resources/app/distdata hin-und-weg-linux-x64/data
 mv hin-und-weg-linux-x64/resources/app/offline hin-und-weg-linux-x64/
 # package distributions
 zip -r "hin&weg-$VERSION-linux.zip" hin-und-weg-linux-x64/
