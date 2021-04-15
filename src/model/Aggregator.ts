a87274227e713f40d04c9b8ed7aa05238322622f
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2021-04-15 09:36:09 +0000

    fix arcane error
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@703 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/model/Aggregator.ts
+++ src/model/Aggregator.ts
@@ -2,111 +2,111 @@ import { Result } from "cubus";
 import R from "ramda";
 
 export type Operation = "Summe" | "Anzahl" | "Saldo" |
-                 "Mittelwert" | "Mittelwert" | "Medianwert" |
-                 "Minimum" | "Maximum" |
-                 "Differenz";
+				 "Mittelwert" | "Mittelwert" | "Medianwert" |
+				 "Minimum" | "Maximum" |
+				 "Differenz";
 
 export type AggregateData = Array<Result<number>>;
 
 // TODO: More than 2 Dimensions (in this case years)
 export default class Aggregator {
 
-    public static create(operation: Operation) {
-        return new Aggregator(operation);
-    }
+	public static create(operation: Operation) {
+		return new Aggregator(operation);
+	}
 
-    public static createForGrouping(operation: Operation, groupByDimension: string) {
-        return this.create(operation).setGroupByDimension(groupByDimension);
-    }
+	public static createForGrouping(operation: Operation, groupByDimension: string) {
+		return this.create(operation).setGroupByDimension(groupByDimension);
+	}
 
-    private groupByDimension: string | null;
-    private operation: string;
-    private defaultValue = R.curry((defaultNum: number, num: number) => isNaN(num) ? defaultNum : num);
-    private rejectNaNs  = R.filter((result: Result<number>) => !isNaN(result.value));
-    private toValues = R.map((result: Result<number>) => result.value);
+	private groupByDimension: string | null;
+	private operation: string;
+	private defaultValue = R.curry((defaultNum: number, num: number) => isNaN(num) ? defaultNum : num);
+	private rejectNaNs  = R.filter((result: Result<number>) => !isNaN(result.value));
+	private toValues = R.map((result: Result<number>) => result.value);
 
-    private constructor(operation: Operation) {
-        this.operation = operation;
-        this.groupByDimension = null;
-    }
+	private constructor(operation: Operation) {
+		this.operation = operation;
+		this.groupByDimension = null;
+	}
 
-    public setGroupByDimension(dimension: string): Aggregator {
-        this.groupByDimension = dimension;
-        return this;
-    }
+	public setGroupByDimension(dimension: string): Aggregator {
+		this.groupByDimension = dimension;
+		return this;
+	}
 
-    public aggregate(data: AggregateData): number | AggregateData {
-       if (this.operation === "Summe" ) {
-           return this.sum(data);
-       }
-       if (this.operation === "Anzahl" ) {
-         return data.length;
-       }
-       if (this.operation === "Saldo" ) {
-        return this.saldo(data);
-       }
-       if (this.operation === "Mittelwert") {
-          return this.mean(data);
-       }
-       if (this.operation === "Medianwert" ) {
-        return this.median(data);
-       }
-       if (this.operation === "Minimum" ) {
-        return this.min(data);
-       }
-       if (this.operation === "Maximum" ) {
-        return this.max(data);
-       }
-       if (this.operation === "Differenz") {
-           return this.diff(data);
-       }
-       return data.length;
-    }
+	public aggregate(data: AggregateData): number | AggregateData {
+		if (this.operation === "Summe" ) {
+			return this.sum(data);
+		}
+		if (this.operation === "Anzahl" ) {
+		 return data.length;
+		}
+		if (this.operation === "Saldo" ) {
+		return this.saldo(data);
+		}
+		if (this.operation === "Mittelwert") {
+		  return this.mean(data);
+		}
+		if (this.operation === "Medianwert" ) {
+		return this.median(data);
+		}
+		if (this.operation === "Minimum" ) {
+		return this.min(data);
+		}
+		if (this.operation === "Maximum" ) {
+		return this.max(data);
+		}
+		if (this.operation === "Differenz") {
+			return this.diff(data);
+		}
+		return data.length;
+	}
 
-    private sum(data: AggregateData) {
-        const values = R.map((result) => this.defaultValue(0, result.value), data);
-        return R.sum(values);
-    }
+	private sum(data: AggregateData) {
+		const values = R.map((result) => this.defaultValue(0, result.value), data);
+		return R.sum(values);
+	}
 
-    private mean(data: AggregateData) {
-        return R.mean(this.toValues(this.rejectNaNs(data)));
-    }
+	private mean(data: AggregateData) {
+		return R.mean(this.toValues(this.rejectNaNs(data)));
+	}
 
-    private median(data: AggregateData) {
-        return R.median(this.toValues(this.rejectNaNs(data)));
-    }
+	private median(data: AggregateData) {
+		return R.median(this.toValues(this.rejectNaNs(data)));
+	}
 
-    private min(data: AggregateData) {
-        const values = this.toValues(this.rejectNaNs(data));
-        return R.reduce(R.min, Number.MAX_VALUE, values);
-    }
+	private min(data: AggregateData): number {
+		const values = this.toValues(this.rejectNaNs(data));
+		return Number(R.reduce(R.min, Number.MAX_VALUE, values));
+	}
 
-    private max(data: AggregateData) {
-        const values = this.toValues(this.rejectNaNs(data));
-        return R.reduce(R.max, Number.MIN_VALUE, values);
-    }
+	private max(data: AggregateData): number {
+		const values = this.toValues(this.rejectNaNs(data));
+		return Number(R.reduce(R.max, Number.MIN_VALUE, values));
+	}
 
-    private saldo(data: AggregateData): AggregateData {
+	private saldo(data: AggregateData): AggregateData {
 
-        return [
-            {
-                property: [
-                    { name: "VonNach", value: "Berlin" },
-                ],
-                value: 0,
-            },
-        ];
-    }
+		return [
+			{
+				property: [
+					{ name: "VonNach", value: "Berlin" },
+				],
+				value: 0,
+			},
+		];
+	}
 
-    private diff(data: AggregateData): AggregateData {
-        return [
-            {
-                property: [
-                    { name: "Von", value: "Berlin" },
-                    { name: "Nach", value: "Paris" },
-                ],
-                value: 0,
-            },
-        ];
-    }
+	private diff(data: AggregateData): AggregateData {
+		return [
+			{
+				property: [
+					{ name: "Von", value: "Berlin" },
+					{ name: "Nach", value: "Paris" },
+				],
+				value: 0,
+			},
+		];
+	}
 }
