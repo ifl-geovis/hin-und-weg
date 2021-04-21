775477c900ef2dea49cd0c4e8a2a787b48ca03c6
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2021-04-21 09:14:25 +0000

    formatting
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@719 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/model/Geodata.ts
+++ src/model/Geodata.ts
@@ -8,78 +8,78 @@ import * as shapefile from "shapefile";
 
 export default class Geodata {
 
-    public static read(path: string,callback: (data: Geodata) => void) {
-        shapefile.read(path, path.replace(".shp", ".dbf"), { encoding: "UTF-8"})
-            .then((data) => {
-                let projection = fs.readFileSync(path.replace(".shp", ".prj")).toString();
-                if (projection == null || projection === undefined) {
-                    projection = Proj4.WGS84;
-                }
-                callback(Geodata.createGeodata(data, projection));
-            });
-    }
+	public static read(path: string,callback: (data: Geodata) => void) {
+		shapefile.read(path, path.replace(".shp", ".dbf"), { encoding: "UTF-8"})
+			.then((data) => {
+				let projection = fs.readFileSync(path.replace(".shp", ".prj")).toString();
+				if (projection == null || projection === undefined) {
+					projection = Proj4.WGS84;
+				}
+				callback(Geodata.createGeodata(data, projection));
+			});
+	}
 
-    private static createGeodata = (features: FeatureCollection<Geometry>, projection: string | Proj4.ProjectionDefinition): Geodata => {
-            return new Geodata(features, projection);
-    }
+	private static createGeodata = (features: FeatureCollection<Geometry>, projection: string | Proj4.ProjectionDefinition): Geodata => {
+			return new Geodata(features, projection);
+	}
 
-    public projection: string | Proj4.ProjectionDefinition;
-    protected featureCollection: FeatureCollection<Geometry>;
+	public projection: string | Proj4.ProjectionDefinition;
+	protected featureCollection: FeatureCollection<Geometry>;
 
-    constructor(featureCollection: FeatureCollection<Geometry>, projection: string|Proj4.ProjectionDefinition) {
-        this.featureCollection = featureCollection;
-        this.projection = projection;
-    }
+	constructor(featureCollection: FeatureCollection<Geometry>, projection: string|Proj4.ProjectionDefinition) {
+		this.featureCollection = featureCollection;
+		this.projection = projection;
+	}
 
-    public count(): number {
-        return R.length(this.featureCollection.features)
-    }
+	public count(): number {
+		return R.length(this.featureCollection.features)
+	}
 
-    public fields(): string[] {
-        assert(R.not(R.isEmpty(this.featureCollection.features)), "data.features is empty");
-        const first = R.head(this.featureCollection.features)!;
-        return R.keys(first.properties) as string[];
-    }
+	public fields(): string[] {
+		assert(R.not(R.isEmpty(this.featureCollection.features)), "data.features is empty");
+		const first = R.head(this.featureCollection.features)!;
+		return R.keys(first.properties) as string[];
+	}
 
-    public attributes(): GeoJsonProperties[] {
-        return R.map((feature) => feature.properties, this.featureCollection.features)
-    }
+	public attributes(): GeoJsonProperties[] {
+		return R.map((feature) => feature.properties, this.featureCollection.features)
+	}
 
-    public getFeatureCollection(): FeatureCollection {
-        return this.featureCollection;
-    }
+	public getFeatureCollection(): FeatureCollection {
+		return this.featureCollection;
+	}
 
-    public getFeatures(): Array<Feature<Geometry, GeoJsonProperties>> {
-        return this.getFeatureCollection().features;
-    }
+	public getFeatures(): Array<Feature<Geometry, GeoJsonProperties>> {
+		return this.getFeatureCollection().features;
+	}
 
-    public getGeometryOf(index: number): Geometry {
-        return this.getFeatureOf(index).geometry;
-    }
+	public getGeometryOf(index: number): Geometry {
+		return this.getFeatureOf(index).geometry;
+	}
 
-    public getFeatureOf(index: number): Feature {
-        assert(this.featureCollection && this.featureCollection.features, "There has to be at least on feature");
-        assert(index >= 0  && index < this.count(), "Index should be between 0 and " + this.count());
-        return this.featureCollection.features[index];
-    }
+	public getFeatureOf(index: number): Feature {
+		assert(this.featureCollection && this.featureCollection.features, "There has to be at least on feature");
+		assert(index >= 0  && index < this.count(), "Index should be between 0 and " + this.count());
+		return this.featureCollection.features[index];
+	}
 
-    public getValueFor(index: number, key: string): number| string| null {
-        const props = this.getFeatureOf(index).properties;
-        if (props && props[key]) {
-            return props[key];
-        }
-        return null;
-    }
+	public getValueFor(index: number, key: string): number| string| null {
+		const props = this.getFeatureOf(index).properties;
+		if (props && props[key]) {
+			return props[key];
+		}
+		return null;
+	}
 
-    public getFeatureByFieldValue(fieldName: string, fieldValue: string|number): Feature {
-        return R.find((feature: Feature) =>
-                feature.properties![fieldName] === fieldValue,this.featureCollection.features)!;
-    }
+	public getFeatureByFieldValue(fieldName: string, fieldValue: string|number): Feature {
+		return R.find((feature: Feature) =>
+				feature.properties![fieldName] === fieldValue,this.featureCollection.features)!;
+	}
 
-    public transformToWGS84(): Geodata {
-        if (this.projection === Proj4.WGS84) {
-            return this;
-        }
-        return new Geodata(reproject.reproject(this.featureCollection, this.projection, Proj4.WGS84), Proj4.WGS84);
-    }
+	public transformToWGS84(): Geodata {
+		if (this.projection === Proj4.WGS84) {
+			return this;
+		}
+		return new Geodata(reproject.reproject(this.featureCollection, this.projection, Proj4.WGS84), Proj4.WGS84);
+	}
 }
