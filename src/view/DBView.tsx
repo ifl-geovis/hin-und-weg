c877a70254656995e8846b8a5e216ca8676efc46
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2021-05-21 08:53:22 +0000

    #2882: add note to database view about NaN
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@740 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/DBView.tsx
+++ src/view/DBView.tsx
@@ -6,50 +6,51 @@ import {InputTextarea} from "primereact/inputtextarea";
 import TableView from "./TableView";
 
 export interface IDBViewProps {
-    db: alaSQLSpace.AlaSQL;
+	db: alaSQLSpace.AlaSQL;
 }
 
 interface IDBViewState {
-    query: string;
+	query: string;
 }
 
 export default class DBView extends React.Component<IDBViewProps, IDBViewState> {
 
-    constructor(props: IDBViewProps) {
-        super(props);
-        this.state = {
-            query: "SELECT * FROM matrices;",
-        };
-    }
+	constructor(props: IDBViewProps) {
+		super(props);
+		this.state = {
+			query: "SELECT * FROM matrices;",
+		};
+	}
 
-    public render(): JSX.Element {
-        let editorContent = this.state.query;
-        let results = [];
-        let errors = "";
-        try {
-            results = this.props.db(this.state.query);
-        } catch (error) {
-            console.error(error);
-            errors = `Error: ${error.message}`;
-            results = [];
-        }
-        return (
-            <div className="p-grid p-justify-around">
-                <div className="p-col-12"> Tabelle <b>matrices</b>:</div>
-                <div className="p-col-12"><TableView items={results} maxRows={20}/></div>
-                <div className="p-col-12"> SQL-Abfrage:</div>
-                <div className="p-col-12">
-                    <InputTextarea className="p-col-12" rows={5}
-                                onChange={(e: React.FormEvent<HTMLTextAreaElement>) => {
-                                    editorContent = (e.target as HTMLTextAreaElement).value;
-                                }}
-                                placeholder={editorContent}
-                                autoResize={true}/>
-                </div>
-                <div className="p-col-2">
-                    <Button label="Ausführen" onClick={(e) => this.setState({query: editorContent})} />
-                </div>
-                <div className="p-col-12" style={{color: "red"}}>{errors}</div>
-            </div>);
-        }
+	public render(): JSX.Element {
+		let editorContent = this.state.query;
+		let results = [];
+		let errors = "";
+		try {
+			results = this.props.db(this.state.query);
+		} catch (error) {
+			console.error(error);
+			errors = `Error: ${error.message}`;
+			results = [];
+		}
+		return (
+			<div className="p-grid p-justify-around">
+				<div className="p-col-12"> Tabelle <b>matrices</b>:</div>
+				<div className="p-col-12"><TableView items={results} maxRows={20}/></div>
+				<div className="p-col-12"> SQL-Abfrage:</div>
+				<div className="p-col-12">
+					<InputTextarea className="p-col-12" rows={5}
+								onChange={(e: React.FormEvent<HTMLTextAreaElement>) => {
+									editorContent = (e.target as HTMLTextAreaElement).value;
+								}}
+								placeholder={editorContent}
+								autoResize={true}/>
+				</div>
+				<div className="p-col-2">
+					<Button label="Ausführen" onClick={(e) => this.setState({query: editorContent})} />
+				</div>
+				<div className="p-col-12" style={{color: "red"}}>{errors}</div>
+				<div className="p-col-12">NaN steht für Not a Number (keine Zahl). Dies betrifft anonymisierte Daten und die Ergebnisse von Berechnungen, die kein Ergebnis haben (wie Division durch Null).</div>
+			</div>);
+		}
 }
