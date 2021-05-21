37ec0a6eec3b440538f14dfd47ecfeb76c03b140
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2021-05-21 14:38:11 +0000

    format
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@753 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/Themes.tsx
+++ src/view/Themes.tsx
@@ -5,37 +5,37 @@ import * as React from "react";
 import { Accordion, AccordionTab } from 'primereact/accordion';
 
 export interface IThemesProps {
-  selected: string;
-  setTheme: (theme: string) => void;
-  themes: string[];
+	selected: string;
+	setTheme: (theme: string) => void;
+	themes: string[];
 }
 
 export default class Themes extends React.Component<IThemesProps> {
 
-  constructor(props: IThemesProps) {
-    super(props);
-    this.makeRadioButton = this.makeRadioButton.bind(this);
-  }
+	constructor(props: IThemesProps) {
+		super(props);
+		this.makeRadioButton = this.makeRadioButton.bind(this);
+	}
 
-  public render(): JSX.Element {
-    const radioButtons = R.map(this.makeRadioButton, this.props.themes);
-    return (
-      <Accordion activeIndex={0}>
-        <AccordionTab header="Thema">
-          {radioButtons}
-        </AccordionTab>
-      </Accordion>
-    );
-  }
+	public render(): JSX.Element {
+		const radioButtons = R.map(this.makeRadioButton, this.props.themes);
+		return (
+			<Accordion activeIndex={0}>
+				<AccordionTab header="Thema">
+					{radioButtons}
+				</AccordionTab>
+			</Accordion>
+		);
+	}
 
-  private makeRadioButton(theme: string): JSX.Element {
-    return (
-      <div key={theme} className="p-col-12">
-        <RadioButton inputId={theme} name="theme" value={theme}
-               onChange={(e) => this.props.setTheme(e.value)} checked={this.props.selected === theme} />
-        <label htmlFor={theme} className="p-radiobutton-label">{theme}</label>
-      </div>
-    );
-  }
+	private makeRadioButton(theme: string): JSX.Element {
+		return (
+			<div key={theme} className="p-col-12">
+				<RadioButton inputId={theme} name="theme" value={theme}
+							 onChange={(e) => this.props.setTheme(e.value)} checked={this.props.selected === theme} />
+				<label htmlFor={theme} className="p-radiobutton-label">{theme}</label>
+			</div>
+		);
+	}
 
 }
