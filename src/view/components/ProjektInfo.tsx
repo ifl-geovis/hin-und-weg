128686c8defc460a6c1ade0f5938308edd39281f
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2021-05-26 09:15:40 +0000

    fix typos
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@779 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/components/ProjektInfo.tsx
+++ src/view/components/ProjektInfo.tsx
@@ -13,10 +13,10 @@ export default class ProjektInfo extends React.Component<{}, {}>
 				<div>
 					hin&weg ist eine Anwendung zur Visualisierung von Umzugsbewegungen. <br/><br/>
 					Hersteller der Software ist das Leibniz Institut für Länderkunde (IfL), Leipzig, www.leibniz-ifl.de.<br/>
-					Die Software wurde vom DELPHI IMM, Potsdam, entwickelt.<br/>
+					Die Software wurde von der DELPHI IMM GmbH in Potsdam entwickelt.<br/>
 					Am Projekt am IfL beteiligt waren: Francis Harvey, Tim Leibert, Eric Losang, Aura Moldovan, Maria Turchenko, Nicola Simon, Rowenia Bender (Stand 4.2021)<br/><br/><br/><br/>
 					<div>
-						Ihre Ansprechpartner für weitere Informationen:<br/><br/>
+						Ihr Ansprechpartner für weitere Informationen:<br/><br/>
 						<strong>Leibniz-Institut für Länderkunde</strong> <br/>
 						Aura Moldovan, A_Moldovan@leibniz-ifl.de , +49 341 600 55 103<br/>
 					</div>
