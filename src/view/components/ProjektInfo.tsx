5472ba595a429db7ae47e60e01ffc38fbc641ea4
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2022-01-25 14:06:46 +0000

    #6365: fix license display in project info
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@1048 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/view/components/ProjektInfo.tsx
+++ src/view/components/ProjektInfo.tsx
@@ -8,11 +8,11 @@ import { TFunction } from "i18next";
 export interface IProjektInfoProps extends WithNamespaces{
 }
 
-// export default 
+// export default
 class ProjektInfo extends React.Component<IProjektInfoProps, {}, {}>
 // class ProjektInfo extends React.Component<{}, {}>
 {
-	
+
 
 	public render(): JSX.Element
 	{
@@ -20,26 +20,20 @@ class ProjektInfo extends React.Component<IProjektInfoProps, {}, {}>
 		return (
 			<div>
 				<h3>{t('projectInfo.title')}</h3> <hr/>
-				{/* <h3>hin&weg Projektinformationen</h3> <hr/> */}
 				<div>
 					{t('projectInfo.text1')} <br/><br/>
-					{/* hin&weg ist eine Anwendung zur Visualisierung von Umzugsbewegungen. <br/><br/> */}
 					{t('projectInfo.text2')}<br/>
-					{/* Hersteller der Software ist das Leibniz Institut für Länderkunde (IfL), Leipzig, www.leibniz-ifl.de.<br/> */}
 					{t('projectInfo.text3')}<br/>
-					{/* Die Software wurde von der DELPHI IMM GmbH in Potsdam entwickelt.<br/> */}
-					{t('projectInfo.text4')}<br/><br/>
-					{/* Am Projekt am IfL beteiligt waren: Francis Harvey, Tim Leibert, Eric Losang, Aura Moldovan, Maria Turchenko, Nicola Simon, Rowenia Bender (Stand 4.2021)<br/><br/><br/><br/> */}
+					{t('projectInfo.text4')}<br/>
+					{t('projectInfo.text5')}<br/><br/>
 					<div>
-						{t('projectInfo.text7')}<br/><br/>
+						<strong>{t('projectInfo.text7')}</strong><br/><br/>
 						{t('projectInfo.text8')}<br/><br/>
 						{t('projectInfo.text9')}<br/><br/>
+						{t('projectInfo.text10')}<br/><br/>
 					</div><br/><br/><br/>
 					<div>
-						{t('projectInfo.text5')}<br/><br/>
-						{/* Ihr Ansprechpartner für weitere Informationen:<br/><br/> */}
-						<strong>{t('projectInfo.text6')}</strong> <br/>
-						{/* <strong>Leibniz-Institut für Länderkunde</strong> <br/> */}
+						{t('projectInfo.text6')}<br/>
 						Aura Moldovan, A_Moldovan@leibniz-ifl.de , +49 341 600 55 103<br/>
 					</div>
 				</div>
