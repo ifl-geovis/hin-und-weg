eeacfe5711323edfb4ef24159ba43d6937b97c4d
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2022-04-10 17:16:11 +0000

    #3451: memorize already loaded stuff
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@1138 366f23da-3ab8-43e7-bb03-06f30a681aac

--- src/project.ts
+++ src/project.ts
@@ -6,9 +6,11 @@ import Log from './log';
 export default class Project {
 
 	private static data: any = JSON.parse('{}');
+	private static loaded: string[] = [];
 
 	public static load(path: string) {
 		try {
+			Project.loaded = [];
 			Project.data = JSON.parse(fs.readFileSync(path, 'utf8'));
 		} catch (e) {
 			Log.debug("Could not load file " + path + ": " + e);
@@ -28,7 +30,17 @@ export default class Project {
 	}
 
 	public static getData(key: string): any {
+		if (Project.isLoaded(key)) return null;
+		// @ts-ignore
+		Project.loaded.push(key);
 		return Project.data[key];
+	}
+
+	private static isLoaded(key: string): boolean {
+		for(const value of Project.loaded) {
+			if (key === value) return true;
+		}
+		return false;
 	}
 
 }
