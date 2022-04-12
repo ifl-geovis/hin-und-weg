3189d90a6b8ee27d095189e44dcabb19c19b9288
jkosche <jkosche@366f23da-3ab8-43e7-bb03-06f30a681aac>
2022-04-12 10:21:00 +0000

    #3451: add ViewData class for storing dashboard view data
    git-svn-id: svn://svn.geoway.de/hin-und-weg/main@1149 366f23da-3ab8-43e7-bb03-06f30a681aac

---
+++ src/data/ViewData.ts
@@ -0,0 +1,24 @@
+import BaseData from './BaseData';
+
+/**
+	Represents the views of the Dashboard.
+ */
+export default class ViewData {
+
+	private basedata: BaseData;
+
+	private dashboards: string[] = [];
+
+	constructor(basedata: BaseData) {
+		this.basedata = basedata;
+	}
+
+	public setDashboardView(index: number, view: string) {
+		this.dashboards[index] = view;
+	}
+
+	public getDashboardView(index: number): string {
+		return this.dashboards[index];
+	}
+
+}
